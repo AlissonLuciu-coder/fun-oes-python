fb053d985588b119443fc04d6eb0524b97c8a8f1
AlissonLuciu-coder <alisson.luciu@gmail.com>
2026-05-27 19:24:51 -0300

    exe03 pronto

---
+++ exe03.py
@@ -0,0 +1,8 @@
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        print ('par')
+    else:
+        print('impar')
+    
+par_ou_impar(10)
+par_ou_impar(3)
---
+++ teste.py
@@ -0,0 +1,2 @@
+par = numero % 2 == 0
+print 
